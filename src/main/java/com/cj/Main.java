af36a0686774b41c758ebf369ee3c087af8a2bea
Chiranjivee Thakur <KH2273@kitspl.com>
2018-08-22 00:57:56 +0530

    Upgrade log4j2 version to 2.8.2

--- src/main/java/com/cj/Main.java
+++ src/main/java/com/cj/Main.java
@@ -2,10 +2,14 @@ package com.cj;
 
 import com.cj.hibernate.HibernateUtil;
 import com.cj.hibernate.Message;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction tx = session.beginTransaction();
@@ -15,5 +19,7 @@ public class Main {
 
         tx.commit();
         session.close();
+        LOGGER.info("Successully executed.");
+        HibernateUtil.shutdown();
     }
 }
